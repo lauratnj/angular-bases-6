09b1931d083e9d791ee0561ad56bff50c2995684
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-09-12 14:50:30 +0200

    Inicio app

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+export class BasicPageComponent {
+
+}
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dynamic-page.component.html',
+  styles: [
+  ]
+})
+export class DynamicPageComponent {
+
+}
