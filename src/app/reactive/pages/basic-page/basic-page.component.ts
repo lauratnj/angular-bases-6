ea605cec5a76dfb850055b28c1b6dea6f2db1288
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-09-15 15:01:05 +0200

    Inicio formulario + Validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  //Construyendo con FormGroup:
+  //public myForm: FormGroup = new FormGroup({
+  //  name: new FormControl('', []),
+  //  price: new FormControl(0),
+  //  inStorage: new FormControl(0),
+  //});
+
+  //Construyendo con FormBuilder:
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3), ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+
+  //Valor de posteo
+  onSave():void {
+
+    if ( this.myForm.invalid ) return;
+
+    console.log(this.myForm.value);
+
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
@@ -15,7 +16,8 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReactiveModule { }
