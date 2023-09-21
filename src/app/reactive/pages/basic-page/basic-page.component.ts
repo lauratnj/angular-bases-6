9cb3a4b8f03d6d0ecc7127382934699520bb9c5b
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-09-21 17:22:39 +0200

    Validaciones dinámicas y eliminar elementos de un FormArray

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   //Construyendo con FormGroup:
   //public myForm: FormGroup = new FormGroup({
@@ -24,13 +30,47 @@ export class BasicPageComponent {
 
   constructor( private fb: FormBuilder ){}
 
+  ngOnInit(): void {
+    //this.myForm.reset( rtx5090 );
+  }
+
+  //Para manejar los errores:
+  isValidField( field:string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+            return 'Este campo es requerido';
+
+        case 'minlength':
+            return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`;
+
+      }
+    }
+
+    return null;
+  }
+
+
 
   //Valor de posteo
   onSave():void {
 
-    if ( this.myForm.invalid ) return;
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, inStorage: 0 });
 
   }
 
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
+
+  // public myForm2 = new FormGroup({
+  //  name: new FormControl('', []),
+  //  favoriteGames: new FormArray([])
+  //});
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3), ]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required ],
+      ['Death Stranding', Validators.required ],
+    ])
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  //Para manejar los errores:
+  isValidField( field:string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+            return 'Este campo es requerido';
+
+        case 'minlength':
+            return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`;
+
+      }
+    }
+
+    return null;
+  }
+
+  onDeleteFavorite( index: number ):void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
